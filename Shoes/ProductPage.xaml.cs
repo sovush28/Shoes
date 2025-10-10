4e16efb566b1a7986c9721a2184c0c90d3d89ae3
Yulia Ignateva <yuliya.ignateva.06@mail.ru>
2025-10-10 21:25:29 +0500

    Добавьте файлы проекта.

---
+++ Shoes/ProductPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shoes
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+
+            var currentProducts = ShoesDE2026Entities.GetContext().Product.ToList();
+
+            ProductListView.ItemsSource = currentProducts;
+
+            UpdateProducts();
+        }
+
+        private void UpdateProducts()
+        {
+            var currentProducts = ShoesDE2026Entities.GetContext().Product.ToList();
+
+            
+
+            ProductListView.ItemsSource = currentProducts;
+        }
+    }
+}
